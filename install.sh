c1b7befb3aafb092462711cb54f7de04dc6a1430
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-06-03 18:09:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-pacman -S - < packages
+echo 'copying pacman.conf'
 cp -r pacman.conf /etc/
+echo 'installing packages.....'
+pacman -Syy - < packages
+
+echo 'rebuilding system, almost done!'
 mkinitcpio -P 
+
+echo 'Have Fun'
 fastfetch
 echo 'All Done! use "sudo reboot" to reboot system and apply changes'
